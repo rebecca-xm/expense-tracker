c1faf61f26d6206eac7546f467870ef73cf40730
Rebecca Mollica <rebecca.mollica@live.it>
2022-04-12 10:48:53 +0200

    feat: add subfolders

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,5 +1,5 @@
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
 import styles from "./Expenses.module.scss";
 
 const Expenses = ({ expenses }) => {
