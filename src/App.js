b4874dacf11830915862d1c22e532e5bcf4813ed
Rebecca Mollica <rebecca.mollica@live.it>
2022-04-13 10:18:39 +0200

    feat: add expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   // Temporary dummy data
@@ -26,6 +27,7 @@ function App() {
 
   return (
     <div className="App">
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
